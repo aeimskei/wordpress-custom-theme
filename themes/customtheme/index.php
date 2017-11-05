2907b82a82c0cee922631afd36c82227adb82fbc
Amy Boudsady <aeimskei@gmail.com>
2017-11-05 20:05:18 +0000

    Add best practices for Head Section that WordPress recommends

--- themes/customtheme/index.php
+++ themes/customtheme/index.php
@@ -1,7 +1,12 @@
 <!doctype html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
-        <title>Custom Title Theme</title>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
     </head>
     <body>
         <?php if (have_posts() ) : ?>
