034bb4fae1b672d6c69eb5e29a5d34d66c394adb
Amy Boudsady <aeimskei@gmail.com>
2017-11-06 22:28:35 +0000

    Create our first template page.php

--- themes/customtheme/index.php
+++ themes/customtheme/index.php
@@ -10,19 +10,19 @@
 
 <?php get_header(); ?>
 
-        <div class="jumbotron gradient-bg margin-neg-20">
-            <div class="container text-center">
-                <h1>Welcome To Our Custom Site</h1>
-            </div>
-        </div>
-        <div class="container">
-            <?php if (have_posts() ) : ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <h2>A singled blog post</h2>
-                    <?php the_title(); ?>
-                    <?php the_content(); ?>
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </div>
+<div class="jumbotron gradient-bg margin-neg-20">
+    <div class="container text-center">
+        <h1>Welcome To Our Custom Site</h1>
+    </div>
+</div>
+<div class="container">
+    <?php if (have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <h2>A singled blog post</h2>
+            <?php the_title(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
         
 <?php get_footer(); ?>
