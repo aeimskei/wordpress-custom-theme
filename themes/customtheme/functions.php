d455af6ae8f7e30cd25bbe35052ecd7ea7e201dd
Amy Boudsady <aeimskei@gmail.com>
2017-11-06 21:50:14 +0000

    Add Bootstrap JS capability and jQuery to solve nav hamburger menu issue when screen shrinks

--- themes/customtheme/functions.php
+++ themes/customtheme/functions.php
@@ -1,21 +1,26 @@
 <?php
 // loading in scripts
 function enqueue_customtheme_styles() {
-    wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
-    wp_enqueue_style("customtheme-style", get_stylesheet_uri() ); //this line of code loads in the "style.css" file
+    // this line of code adds Twitter Bootstrap's JavaScript library
+    // jQuery placed here to make sure loads before others
+    wp_enqueue_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array('jquery'));
+    // this line of code gives us access to the Twitter Boostrap library
+    wp_enqueue_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
+    //this line of code loads in the "style.css" file
+    wp_enqueue_style('customtheme-style', get_stylesheet_uri() );
 }
-add_action("wp_enqueue_scripts", "enqueue_customtheme_styles");
+add_action('wp_enqueue_scripts', 'enqueue_customtheme_styles');
 
 // this block will allow us to register a custom menu for our Admin section
 function customtheme_setup() {
     // register a custom primary navigation menu
     register_nav_menus( array(
-        "primary" => __( "Primary Menu", "customtheme" )
+        'primary' => __( 'Primary Menu', 'customtheme' )
     ));
     
     // add theme support for document Title tag
-    add_theme_support("title-tag");
+    add_theme_support('title-tag');
 }
-add_action( "after_setup_theme", "customtheme_setup" );
+add_action( 'after_setup_theme', 'customtheme_setup' );
 
 ?>
--- themes/customtheme/header.php
+++ themes/customtheme/header.php
@@ -12,7 +12,7 @@
         <nav class="navbar gradient-bg main-custom-nav">
             <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -23,7 +23,7 @@
                 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Link</a></li>
+                        <!-- injection code for our dynamic navigation menu -->
                         <?php
                             wp_nav_menu( array(
                                 'theme_location' => 'primary',
